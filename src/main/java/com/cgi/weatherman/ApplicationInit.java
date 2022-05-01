cd02bdf7c08c5d5eb2e89341403c33a239b96fb6
Danila Romanov <danila.romanof@gmail.com>
2022-05-01 05:09:07 +0300

    SourceApiKey logic has been deleted

--- src/main/java/com/cgi/weatherman/ApplicationInit.java
+++ src/main/java/com/cgi/weatherman/ApplicationInit.java
@@ -1,30 +1,14 @@
 package com.cgi.weatherman;
 
 import com.cgi.weatherman.builder.WebClientBuilder;
-import com.cgi.weatherman.model.SourceApiKey;
-import com.cgi.weatherman.repository.SourceApiKeyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Component
 public class ApplicationInit implements CommandLineRunner {
-    @Autowired
-    SourceApiKeyRepository sourceApiKeyRepository;
-
     @Override
     public void run(String... args) throws Exception {
         WebClient webClient = WebClientBuilder.getWebClient("http://api.weatherapi.com/v1/current.json?key=40377b1c488e4a21a83190020221304&q=London&aqi=no");
-
-        SourceApiKey sourceApiKey = new SourceApiKey("test", "http://api.weatherapi.com/v1/current.json?key=40377b1c488e4a21a83190020221304&q=London&aqi=no");
-        SourceApiKey sourceApiKey1 = new SourceApiKey("test1", "HRw2sGdd4lT0CU7y8JHTHLFLPjBfTFg4");
-
-        List<SourceApiKey> keyApiList = new ArrayList<>(Arrays.asList(sourceApiKey1, sourceApiKey));
-        sourceApiKeyRepository.saveAll(keyApiList);
     }
 }
--- src/main/java/com/cgi/weatherman/service/WeatherService.java
+++ src/main/java/com/cgi/weatherman/service/WeatherService.java
@@ -1,8 +1,6 @@
 package com.cgi.weatherman.service;
 
-import com.cgi.weatherman.model.SourceApiKey;
 import com.cgi.weatherman.model.WeatherApi;
-import com.cgi.weatherman.repository.SourceApiKeyRepository;
 import com.cgi.weatherman.repository.WeatherRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +9,13 @@ import java.util.List;
 @Service
 public class WeatherService {
     final WeatherRepository weatherRepository;
-    final SourceApiKeyRepository sourceApiKeyRepository;
 
-    public WeatherService(WeatherRepository weatherRepository, SourceApiKeyRepository sourceApiKeyRepository) {
+    public WeatherService(WeatherRepository weatherRepository) {
         this.weatherRepository = weatherRepository;
-        this.sourceApiKeyRepository = sourceApiKeyRepository;
     }
 
     public List<WeatherApi> getWeathers() {
         return weatherRepository.getAllWeathers();
     }
 
-    public List<SourceApiKey> getKeys() {
-        return sourceApiKeyRepository.getAllSourceApiKeys();
-    }
 }
