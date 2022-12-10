8f097e690227519efee8e213aa6f2daf5ba11c09
Danila Romanov <danila.romanof@gmail.com>
2022-12-10 16:08:01 +0200

    UserUtil update

--- src/main/java/com/cgi/weatherman/security/UserUtil.java
+++ src/main/java/com/cgi/weatherman/security/UserUtil.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class UserUtil {
-
     public static Object getLoggedInUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) return null;
